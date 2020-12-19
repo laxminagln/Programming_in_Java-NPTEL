97344c0c4fd4c81b349ad3a89679ba3b28f3a9c0
nagln <laxminagln@gmail.com>
2020-12-19 19:51:20 +0530

    Create Assignment5.java

---
+++ Week-2/Assignment5.java
@@ -0,0 +1,9 @@
+public class Question215{ 
+    public static void main(String[] args) { 
+    String nptel, space, java;
+    nptel = "NPTEL";
+    space = " ";
+    java = "JAVA";
+    System.out.print(nptel+space+java);
+   }
+}
