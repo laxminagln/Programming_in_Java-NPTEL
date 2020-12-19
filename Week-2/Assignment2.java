eba557772112e450b1ab5147e0cad8dec224b4fd
nagln <laxminagln@gmail.com>
2020-12-19 19:48:47 +0530

    Create Assignment2.java

---
+++ Week-2/Assignment2.java
@@ -0,0 +1,16 @@
+class Printer { 
+    public void print() { 
+		System.out.println("Hi! I class SCHOOL."); 
+    } 
+    public void print(String s) { 
+		System.out.println(s); 
+    } 
+} 
+
+public class Question22{ 
+    public static void main(String[] args) {    
+    Printer p = new Printer();
+    p.print("Hi! I am class STUDENT");
+    p.print();
+    }
+}
