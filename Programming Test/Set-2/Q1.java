22305e93547a188e272a81174f1ab1c37a1840aa
nagln <laxminagln@gmail.com>
2020-12-30 15:02:06 +0530

    Create Q1.java

---
+++ Programming Test/Set-2/Q1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Question1 {
+    public static void main(String[] args) {
+
+	Scanner sc = new Scanner(System.in);
+	int n=sc.nextInt();
+	int sum=0;
+	int result=1;
+    int i=0;
+  while(result<=n)
+	 {
+	  if(i%2!=0)
+	  {
+		 sum=sum+i;
+		 result=result+1; 
+	   }			
+		i=i+1; 
+	  }
+	  System.out.print(sum);
+	 }
+    }
